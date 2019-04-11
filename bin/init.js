3ca5471a5c7b82a79706fb589e21ae4e7bf49b23
Claim <ywtyx01@126>
2019-04-11 17:52:47 +0800

    add default user creating action

--- bin/init.js
+++ bin/init.js
@@ -1,12 +1,10 @@
 const pendingModel = require('../src/models');
 
-pendingModel.then(model => {
-  model.database.sync({force: (process.argv[2] || '').includes('-f')})
-    .then(() => {
-      return model.user.findOrCreate({
-        where: {username: 'root'},
-        defaults: {password: 'default', description: 'Administrator of the application'},
-      });
-    })
-    .then(() => process.exit(0));
-});
+pendingModel.then(model => model.database.sync({force: (process.argv[2] || '').includes('-f')}))
+  .then(() => {
+    return model.user.findOrCreate({
+      where: {username: 'root'},
+      defaults: {password: 'default', description: 'Administrator of the application'},
+    });
+  })
+  .then(() => process.exit(0));
